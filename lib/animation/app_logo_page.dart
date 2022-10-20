ed2fd6e33942c607aea0af26d6f6f81f72a85d88
贾元发 <jiayuanfa@yeah.net>
2022-10-20 14:48:03 +0800

    Flutter动画

--- lib/animation/app_logo_page.dart
+++ lib/animation/app_logo_page.dart
@@ -17,8 +17,11 @@ class _AppLogoAnimationState extends State<AppLogoAnimation>
   @override
   void initState() {
     super.initState();
+    /// 动画控制器
     animationController =
         AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    /// 动画参数范围设定器
+    /// 使用动画参数设定器+动画控制器初始化一个Animation
     animation = Tween<double>(begin: 0, end: 300).animate(animationController!)
       ..addListener(() {
         /// 动画每调用一次就会回调我们的监听器
@@ -35,6 +38,15 @@ class _AppLogoAnimationState extends State<AppLogoAnimation>
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation'),
+      ),
+      body: _contentWidget(),
+    );
+  }
+
+  _contentWidget() {
     return Container(
       padding: const EdgeInsets.all(50),
       decoration: const BoxDecoration(color: Colors.white),
@@ -51,8 +63,11 @@ class _AppLogoAnimationState extends State<AppLogoAnimation>
             'State: ${animationStatus.toString()}',
             style: const TextStyle(fontSize: 20),
           ),
-          Text('Value: ${animationValue.toString()}',
-              style: const TextStyle(fontSize: 20)),
+          Text(
+              'Value: ${animationValue.toString()}',
+              style: const TextStyle(
+                fontSize: 20,
+              )),
           SizedBox(
             width: animation?.value,
             height: animation?.value,
