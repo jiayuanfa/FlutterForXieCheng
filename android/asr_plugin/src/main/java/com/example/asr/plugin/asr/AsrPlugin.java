586fef7f121d5c50429342e336a8e66a2a882960
贾元发 <jiayuanfa@yeah.net>
2022-11-10 18:46:02 +0800

    语音SDK配置

---
+++ android/asr_plugin/src/main/java/com/example/asr/plugin/asr/AsrPlugin.java
@@ -0,0 +1,16 @@
+package com.example.asr.plugin.asr;
+
+import androidx.annotation.NonNull;
+
+import java.lang.invoke.MethodHandle;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+public class AsrPlugin implements MethodChannel.MethodCallHandler {
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall methodCall, @NonNull MethodChannel.Result result) {
+
+    }
+}
